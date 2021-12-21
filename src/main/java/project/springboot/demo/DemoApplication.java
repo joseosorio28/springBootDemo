ea8b2a310a6499c0f937ca52bb5d407a29f81782
joseosorio28 <joseosorio28@hotmail.com>
2021-12-21 18:33:07 -0500

    Added model: student class. and test code on DemoApplication.java

--- src/main/java/project/springboot/demo/DemoApplication.java
+++ src/main/java/project/springboot/demo/DemoApplication.java
@@ -2,12 +2,31 @@ package project.springboot.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import project.springboot.demo.student.Student;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping
+	public List<Student> hello(){
+		return List.of(
+				new Student(
+						1L,
+						"Jose Osorio",
+						"joseosorio@mail.com",
+						32,
+						LocalDate.of(1989,9,8)
+						));
 	}
 
 }
