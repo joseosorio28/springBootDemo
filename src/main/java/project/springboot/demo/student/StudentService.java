f54d4acac4dd46db97f02a0e988c61734d123c41
joseosorio28 <joseosorio28@hotmail.com>
2021-12-21 22:12:36 -0500

    Added  Repository layer : StudentRepository.java

--- src/main/java/project/springboot/demo/student/StudentService.java
+++ src/main/java/project/springboot/demo/student/StudentService.java
@@ -1,20 +1,21 @@
 package project.springboot.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @Service
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents(){
-        return List.of(new Student(
-                        1L,
-                        "Jose Osorio",
-                        "joseosorio@mail.com",
-                        32,
-                        LocalDate.of(1989,9,8)
-                ));
+        return studentRepository.findAll();
     }
 }
